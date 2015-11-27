de738b86c12fffdc30ea33e7a27867c91336ec2b
Adam Alpern <adam.alpern@gmail.com>
2015-11-27 14:27:46 -0800

    Filling in more api GET methods

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 A utility for archiving your Pinterest boards for offline access. Uses
 the [offical Pinterest API](https://developers.pinterest.com/docs/getting-started/introduction/).
+
+### API Client
+
+#### Implemented methods
+
+* [x] `GET /v1/me/`
+* [x] `GET /v1/me/pins/`
+* [x] `GET /v1/me/likes/`
+* [x] `GET /v1/me/boards/`
+* [x] `GET /v1/me/boards/suggested/`
+* [x] `GET /v1/me/followers/`
+* [x] `GET /v1/me/following/boards/`
+* [x] `GET /v1/me/following/users/`
+* [x] `GET /v1/pins/<id>/`
+* [x] `GET /v1/boards/<board-spec>/pins/`
+* [x] `GET /v1/boards/<board-spec>/pins/`
--- src/ts/pinterest/client.ts
+++ src/ts/pinterest/client.ts
@@ -103,6 +103,46 @@ export class Client implements Fetcher {
   }
 
   /**
+   * Get the current logged in user's suggested boards.
+   */
+  async get_suggested_boards(limit?: number) : Promise<PagedResponse<model.Board>> {
+    return this.get(`${Constants.API_ROOT}/me/boards/suggested/`, {
+      fields: model.BoardFields.join(','),
+      limit: limit
+    }).then(data => new PagedResponse<model.Board>(this, data))
+  }
+
+  /**
+   * Get the boards that the current logged in user follows.
+   */
+  async get_following_boards(limit?: number) : Promise<PagedResponse<model.Board>> {
+    return this.get(`${Constants.API_ROOT}/me/following/boards/`, {
+      fields: model.BoardFields.join(','),
+      limit: limit
+    }).then(data => new PagedResponse<model.Board>(this, data))
+  }
+
+  /**
+   * Get the current logged in user's liked pins.
+   */
+  async get_liked_pins(limit?: number) : Promise<PagedResponse<model.Pin>> {
+    return this.get(`${Constants.API_ROOT}/me/likes/`, {
+      fields: model.PinFields.join(','),
+      limit: limit
+    }).then(data => new PagedResponse<model.Pin>(this, data))
+  }
+
+  /**
+   * Get all of the current logged in user's pins.
+   */
+  async get_all_pins(limit?: number) : Promise<PagedResponse<model.Pin>> {
+    return this.get(`${Constants.API_ROOT}/me/pins/`, {
+      fields: model.PinFields.join(','),
+      limit: limit
+    }).then(data => new PagedResponse<model.Pin>(this, data))
+  }
+
+  /**
    * Get a specific board's pins.
    */
   async get_pins(board: model.Board|string, limit?: number) : Promise<PagedResponse<model.Pin>> {
@@ -112,5 +152,41 @@ export class Client implements Fetcher {
     return this.get(`${Constants.API_ROOT}/boards/${id}/pins/`, {
       fields: model.PinFields.join(',')
     }).then(data => new PagedResponse<model.Pin>(this, data))
+  }
+
+  /**
+   * Get a specific pin.
+   */
+  async get_pin(id: string) : Promise<model.Pin> {
+    return this.get(`${Constants.API_ROOT}/pins/${id}/`, {
+      fields: model.PinFields.join(',')
+    })
+  }
+
+  /**
+   * Get a specific board.
+   */
+  async get_board(board_spec: string) : Promise<model.Board> {
+    return this.get(`${Constants.API_ROOT}/boards/${board_spec}/`, {
+      fields: model.BoardFields.join(',')
+    })
+  }
+
+  /**
+   * Get the current logged in user's followers.
+   */
+  async get_followers(limit?: number) : Promise<PagedResponse<model.User>> {
+    return this.get(`${Constants.API_ROOT}/me/followers/`, {
+      fields: model.UserFields.join(',')
+    }).then(data => new PagedResponse<model.User>(this, data))
+  }
+
+  /**
+   * Get the users that the current logged in user follows.
+   */
+  async get_following(limit?: number) : Promise<PagedResponse<model.User>> {
+    return this.get(`${Constants.API_ROOT}/me/following/users/`, {
+      fields: model.UserFields.join(',')
+    }).then(data => new PagedResponse<model.User>(this, data))
   }
 }
